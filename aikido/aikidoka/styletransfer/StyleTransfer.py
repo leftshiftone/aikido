b693530addcc2b7098bc01f223eca0a0902ed164
christian <christian.weber@leftshift.one>
2020-03-22 00:44:04 +0100

    StyleTransferAikidoka modified

--- aikido/aikidoka/styletransfer/StyleTransfer.py
+++ aikido/aikidoka/styletransfer/StyleTransfer.py
@@ -1,5 +1,3 @@
-import copy
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -11,192 +9,166 @@ from aikido.dojo.listener import SeedListener, BackendListener
 
 Image.MAX_IMAGE_PIXELS = 1000000000  # Support gigapixel images
 
-from aikido.aikidoka.styletransfer.CaffeLoader import loadCaffemodel, ModelParallel
+from aikido.aikidoka.styletransfer.VGG19 import loadVGG19
 
 from aikido.aikidoka.styletransfer.StyleTransferKun import StyleTransferKun
 from aikido.nn.modules.styletransfer.ContentLoss import ContentLoss
 from aikido.nn.modules.styletransfer.StyleLoss import StyleLoss
 from aikido.nn.modules.styletransfer.TVLoss import TVLoss
-from aikido.nn.modules.styletransfer.preprocessing import preprocess
-from aikido.__common__.io.Files import get_files
+# from aikido.nn.modules.styletransfer.preprocessing import preprocess
+from aikido.nn.modules.styletransfer.fileloader import preprocess
+from aikido.nn.modules.styletransfer.Normalization import Normalization
 
 
+# https://arxiv.org/pdf/1605.04603.pdf
 class StyleTransfer(Aikidoka):
 
     def __init__(self, kun: StyleTransferKun):
         super().__init__()
-        self.kun = kun
 
-    def forward(self):
-        BackendListener(self.kun.backend, self.kun.cudnn_autotune).training_started(None, None, None)
+        cnn, layerList = loadVGG19(kun)
 
-        cnn, layerList = loadCaffemodel(self.kun.model_file, self.kun.pooling, self.kun.gpu, self.kun.disable_check)
+        content_image = preprocess(kun.content_image, kun.image_size, kun).type(kun.get_dtype())
+        styling_image = preprocess(kun.styling_image, kun.image_size, kun).type(kun.get_dtype())
 
-        content_image = preprocess(self.kun.content_image, self.kun.image_size).type(self.kun.get_dtype())
-        style_image_list = get_files(self.kun.style_image.split(','))
-
-        style_images_caffe = []
-        for image in style_image_list:
-            style_size = int(self.kun.image_size * self.kun.style_scale)
-            img_caffe = preprocess(image, style_size).type(self.kun.get_dtype())
-            style_images_caffe.append(img_caffe)
-
-        # Handle style blending weights for multiple style inputs
-        style_blend_weights = self.kun.get_style_blend_weights(style_image_list)
+        # assert styling_image.size() == content_image.size(), "content/styling image size mismatch"
 
         # Set up the network, inserting style and content loss modules
-        cnn = copy.deepcopy(cnn)
-        content_losses, style_losses, tv_losses = [], [], []
-        next_content_idx, next_style_idx = 1, 1
-        net = nn.Sequential()
-        c, r = 0, 0
-        if self.kun.tv_weight > 0:
-            tv_mod = TVLoss(self.kun.tv_weight).type(self.kun.get_dtype())
-            net.add_module(str(len(net)), tv_mod)
+        content_losses, styling_losses, tv_losses = [], [], []
+        net = nn.Sequential(Normalization().to(get_device()))
+
+        if kun.tv_weight > 0:
+            tv_mod = TVLoss().type(kun.get_dtype())
+            net.add_module("tv_loss", tv_mod)
             tv_losses.append(tv_mod)
 
-        for i, layer in enumerate(list(cnn), 1):
-            if next_content_idx <= len(self.kun.content_layers) or next_style_idx <= len(self.kun.style_layers):
-                if isinstance(layer, nn.Conv2d):
-                    net.add_module(str(len(net)), layer)
+        conv, relu, pool, norm = 0, 0, 0, 0
+        for layer in cnn.children():
+            add_content_loss = False
+            add_styling_loss = False
 
-                    if layerList['C'][c] in self.kun.content_layers:
-                        print("Setting up content layer " + str(i) + ": " + str(layerList['C'][c]))
-                        target = net(content_image).detach()
-                        loss_module = ContentLoss(target, self.kun.content_weight)
-                        net.add_module(str(len(net)), loss_module)
-                        content_losses.append(loss_module)
+            if isinstance(layer, nn.Conv2d):
+                add_content_loss = layerList['C'][conv] in kun.content_layers
+                add_styling_loss = layerList['C'][conv] in kun.styling_layers
+                name = layerList["C"][conv]
+                conv += 1
+            elif isinstance(layer, nn.ReLU):
+                add_content_loss = layerList['R'][relu] in kun.content_layers
+                add_styling_loss = layerList['R'][relu] in kun.styling_layers
+                name = layerList["R"][relu]
+                relu += 1
+                layer = nn.ReLU(inplace=False)
+            elif isinstance(layer, nn.MaxPool2d):
+                name = layerList["P"][pool]
+                pool += 1
+            elif isinstance(layer, nn.BatchNorm2d):
+                name = 'bn_{}'.format(norm)
+                norm += 1
+            else:
+                raise RuntimeError('Unrecognized layer: {}'.format(layer.__class__.__name__))
 
-                    if layerList['C'][c] in self.kun.style_layers:
-                        print("Setting up style layer " + str(i) + ": " + str(layerList['C'][c]))
-                        loss_module = StyleLoss(self.kun.style_weight)
-                        net.add_module(str(len(net)), loss_module)
-                        style_losses.append(loss_module)
-                    c += 1
+            net.add_module(name, layer)
 
-                if isinstance(layer, nn.ReLU):
-                    net.add_module(str(len(net)), layer)
+            if add_content_loss:
+                target = net(content_image).detach()
+                content_loss = ContentLoss(target)
+                content_losses.append(content_loss)
+                net.add_module("content_loss_{}".format(len(content_losses)), content_loss)
 
-                    if layerList['R'][r] in self.kun.content_layers:
-                        print("Setting up content layer " + str(i) + ": " + str(layerList['R'][r]))
-                        target = net(content_image).detach()
-                        loss_module = ContentLoss(target, self.kun.content_weight)
-                        net.add_module(str(len(net)), loss_module)
-                        content_losses.append(loss_module)
-                        next_content_idx += 1
+            if add_styling_loss:
+                target_feature = net(styling_image).detach()
+                styling_loss = StyleLoss(target_feature)
+                styling_losses.append(styling_loss)
+                net.add_module("style_loss_{}".format(len(styling_losses)), styling_loss)
 
-                    if layerList['R'][r] in self.kun.style_layers:
-                        print("Setting up style layer " + str(i) + ": " + str(layerList['R'][r]))
-                        loss_module = StyleLoss(self.kun.style_weight)
-                        net.add_module(str(len(net)), loss_module)
-                        style_losses.append(loss_module)
-                        next_style_idx += 1
-                    r += 1
+        # now we trim off the layers after the last content and style losses
+        for i in range(len(net) - 1, -1, -1):
+            if isinstance(net[i], ContentLoss) or isinstance(net[i], StyleLoss):
+                net = net[:(i + 1)]
+                break
 
-                if isinstance(layer, nn.MaxPool2d) or isinstance(layer, nn.AvgPool2d):
-                    net.add_module(str(len(net)), layer)
+        print(net)
 
-        if self.kun.is_multidevice():
-            net = self.setup_multi_device(net, self.kun)
-
-        # Capture style targets
-        for i in content_losses:
-            i.mode = 'None'
-
-        for i, image in enumerate(style_images_caffe):
-            print("Capturing style target " + str(i + 1))
-            for j in style_losses:
-                j.mode = 'capture'
-                j.blend_weight = style_blend_weights[i]
-            net(style_images_caffe[i])
-
-        # Set all loss modules to loss mode
-        for i in content_losses:
-            i.mode = 'loss'
-        for i in style_losses:
-            i.mode = 'loss'
-
-        # Maybe normalize content and style weights
-        if self.kun.normalize_weights:
-            normalize_weights(content_losses, style_losses)
-
-        # Freeze the network in order to prevent
-        # unnecessary gradient calculations
         for param in net.parameters():
             param.requires_grad = False
 
-        # Initialize the image
+        self.kun = kun
+        self.net = net
+        self.content_image = content_image
+        self.styling_image = styling_image
+        self.content_losses, self.styling_losses, self.tv_losses = content_losses, styling_losses, tv_losses
+
+    def forward(self):
+        BackendListener(self.kun.backend, self.kun.cudnn_autotune).training_started(None, None, None)
         SeedListener(self.kun.seed).training_started(None, None, None)
-        img = nn.Parameter(self.init_image(content_image))
+
+        generated_image = self.init_image()
+        generated_image = nn.Parameter(generated_image)
+        optimizer = self.setup_optimizer(generated_image, self.kun)
 
         # Function to evaluate loss and gradient. We run the net forward and
         # backward to get the gradient, and sum up losses from the loss modules.
         # optim.lbfgs internally handles iteration and calls this function many
         # times, so we manually count the number of iterations to handle printing
         # and saving intermediate results.
-        num_calls = [0]
+        dan = [0]
 
-        def feval():
-            optimizer.zero_grad()
-            net(img)
-            loss = 0
+        while dan[0] <= self.kun.dans:
+            def closure():
+                optimizer.zero_grad()
+                self.net(generated_image)
 
-            for mod in content_losses:
-                loss += mod.loss.to(self.kun.get_backward_device())
-            for mod in style_losses:
-                loss += mod.loss.to(self.kun.get_backward_device())
-            if self.kun.tv_weight > 0:
-                for mod in tv_losses:
-                    loss += mod.loss.to(self.kun.get_backward_device())
+                content_score, styling_score, tv_score = 0, 0, 0
 
-            loss.backward()
+                for sl in self.styling_losses:
+                    styling_score += sl.loss.to(self.kun.get_backward_device())
+                for cl in self.content_losses:
+                    content_score += cl.loss.to(self.kun.get_backward_device())
+                for tl in self.tv_losses:
+                    tv_score += tl.loss.to(self.kun.get_backward_device())
 
-            listener = CheckpointListener(self.kun)
+                styling_score *= self.kun.styling_weight
+                content_score *= self.kun.content_weight
+                tv_score *= self.kun.tv_weight
 
-            listener.maybe_save(num_calls[0], img, content_image)
-            listener.maybe_print(num_calls[0], loss, content_losses, style_losses)
+                loss = styling_score + content_score + tv_score
+                loss.backward()
 
-            num_calls[0] += 1
-            return loss
+                listener = CheckpointListener(self.kun)
 
-        optimizer = self.setup_optimizer(img, self.kun)
-        while num_calls[0] <= self.kun.dans:
-            optimizer.step(feval)
+                listener.maybe_save(dan[0], generated_image, self.content_image)
+                listener.maybe_print(dan[0], loss, self.content_losses, self.styling_losses, self.tv_losses)
+
+                dan[0] += 1
+                return styling_score + content_score + tv_score
+
+            optimizer.step(closure)
+
+        # generated_image.data.clamp_(0, 1)
+        return generated_image
 
     # Configure the optimizer
     def setup_optimizer(self, img, kun):
         print("Running optimization with L-BFGS")
         optim_state = {
-            'tolerance_change': -1,
-            'tolerance_grad': -1,
-            'history_size':kun.lbfgs_num_correction
+            'history_size': kun.lbfgs_num_correction
         }
-        optimizer = optim.LBFGS([img], **optim_state)
-        return optimizer
+        return optim.LBFGS([img.requires_grad_()], **optim_state)
 
-    def init_image(self, content_image):
+    def init_image(self):
         if self.kun.init == 'random':
-            B, C, H, W = content_image.size()
-            return torch.randn(C, H, W).mul(0.001).unsqueeze(0).type(self.kun.get_dtype())
+            B, C, H, W = self.content_image.size()
+            factor = 0.001 if self.kun.caffee_model else 1
+
+            return torch.randn(C, H, W).mul(factor).unsqueeze(0).type(self.kun.get_dtype())
         elif self.kun.init == 'image':
             if self.kun.init_image != None:
-                image_size = (content_image.size(2), content_image.size(3))
+                image_size = (self.content_image.size(2), self.content_image.size(3))
                 init_image = preprocess(self.kun.init_image, image_size).type(self.kun.get_dtype())
                 return init_image.clone()
             else:
-                return content_image.clone()
-
-    def setup_multi_device(self, net, kun):
-        assert len(kun.gpu.split(',')) - 1 == len(kun.multidevice_strategy.split(',')), \
-            "The number of -multidevice_strategy layer indices minus 1, must be equal to the number of -gpu devices."
-
-        new_net = ModelParallel(net, kun.gpu, kun.multidevice_strategy)
-        return new_net
+                return self.content_image.clone()
 
 
-# Divide weights by channel size
-def normalize_weights(content_losses, style_losses):
-    for n, i in enumerate(content_losses):
-        i.strength = i.strength / max(i.target.size())
-    for n, i in enumerate(style_losses):
-        i.strength = i.strength / max(i.target.size())
+def get_device():
+    return torch.device("cuda" if torch.cuda.is_available() else "cpu")
